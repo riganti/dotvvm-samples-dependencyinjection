231f45113b6642ac8508cf7efee89e0712c4f982
Tomáš Herceg <tomas.herceg@riganti.cz>
2018-07-21 20:32:46 +0200

    Folder structure changed

---
+++ Autofac/Owin/OWINAutofacSample/AutofacViewModelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web;
+using Autofac;
+using DotVVM.Framework.Hosting;
+using DotVVM.Framework.ViewModel.Serialization;
+
+namespace OWINAutofacSample
+{
+
+    public class AutofacViewModelLoader : DefaultViewModelLoader
+    {
+        private readonly IContainer container;
+
+        public AutofacViewModelLoader(IContainer container)
+        {
+            this.container = container;
+        }
+
+        protected override object CreateViewModelInstance(Type viewModelType, IDotvvmRequestContext context)
+        {
+            var scope = AutofacUtils.GetOrCreateScope(context, container);
+            return scope.Resolve(viewModelType);
+        }
+
+        public override void DisposeViewModel(object instance)
+        {
+            var context = HttpContext.Current.GetOwinContext().GetDotvvmContext();
+            AutofacUtils.DisposeScope(context);
+        }
+    }
+
+}
